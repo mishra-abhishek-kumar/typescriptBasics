a1d6155d0ab65d1c3f6e24860dae5b6e5ef0a8af
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2024-03-07 15:38:31 +0530

    working with array type

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const num1Element = document.getElementById("num1");
 const num2Element = document.getElementById("num2");
 const buttonElement = document.querySelector("button");
+const numResults = [];
+const textResults = [];
 function add(num1, num2) {
     if (typeof num1 === "number" && typeof num2 == "number") {
         return num1 + num2;
@@ -18,8 +20,9 @@ buttonElement.addEventListener("click", () => {
     const num1 = num1Element.value;
     const num2 = num2Element.value;
     const result = add(+num1, +num2);
+    numResults.push(result);
     const stringResult = add(num1, num2);
-    console.log(result);
-    console.log(stringResult);
+    textResults.push(stringResult);
     printResult({ val: result, timestamp: new Date() });
+    console.log(numResults, textResults);
 });
